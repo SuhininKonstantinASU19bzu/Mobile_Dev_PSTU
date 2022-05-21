ab4f25413eb60786bc6eff1037e5b889d4e4f829
SuhininKonstantinASU19bzu <65463165+SuhininKonstantinASU19bzu@users.noreply.github.com>
2022-05-21 18:05:15 +0500

    Animation start added

--- BSOD/app/src/main/java/com/example/bsod/MainActivity.java
+++ BSOD/app/src/main/java/com/example/bsod/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.bsod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable bsodAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.bsod);
+        myImageView.setBackgroundResource(R.drawable.animation_bsod);
+        bsodAnimation = (AnimationDrawable) myImageView.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        bsodAnimation.start();
     }
 }
